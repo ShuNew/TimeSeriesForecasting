d76d2bb9e05b54383f4c84cefe2a4c29229935b2
Shubham Kundu <71392066+ShuNew@users.noreply.github.com>
2022-02-10 17:18:47 -0500

    Generalized for Different Time Series

--- InformationAPIs/AlphaVantage.py
+++ InformationAPIs/AlphaVantage.py
@@ -24,15 +24,38 @@ def getData(query_switch, function, symbol, interval, *optionalparams):
     ### Get data using AlphaVantage API
     # requires an API key in a text file "AlphaVantage_apikey.txt" located in the same directory as "AlphaVantage.py"
     query_switch: bool, if True then query AlphaVantage and save .csv file, if False then read saved .csv file
-    function, symbol, interval, *optionalparams: see AlphaVantage documentation
+    (function, symbol, interval, *optionalparams): see AlphaVantage documentation
     '''
     file_name = str(function)+'-'+str(symbol)+'-'+str(interval)+'.csv'
     fullfilename = pathmaker(file_name)
     if query_switch == True:
         if optionalparams:
-            url = 'https://www.alphavantage.co/query?'+'function='+str(function)+'&'+'symbol='+str(symbol)+'&'+'interval='+str(interval)+'&'+'apikey='+str(apikey)+'&datatype=csv'+str(optionalparams)
+            url = 'https://www.alphavantage.co/query?'+'function='+str(function)+'&'+'symbol='+str(symbol)+'&'+'interval='+str(interval)+str(optionalparams[0])+'&'+'apikey='+str(apikey)+'&datatype=csv'
         else:
             url = 'https://www.alphavantage.co/query?'+'function='+str(function)+'&'+'symbol='+str(symbol)+'&'+'interval='+str(interval)+'&'+'apikey='+str(apikey)+'&datatype=csv'
+        data = pd.read_csv(url)
+        data.to_csv(fullfilename, index=False)
+    if query_switch == False:
+        data = pd.DataFrame(pd.read_csv(fullfilename))
+    data["timestamp"] = pd.to_datetime(data["timestamp"])
+    data = data.sort_values(by="timestamp")
+    data = data.reset_index(drop=True)
+    return(data)
+
+def getDataGeneral(query_switch, file, function, symbol, *optionalparams):
+    '''
+    ### Get data using AlphaVantage API
+    # requires an API key in a text file "AlphaVantage_apikey.txt" located in the same directory as "AlphaVantage.py"
+    query_switch: bool, if True then query AlphaVantage and save .csv file, if False then read saved .csv file
+    file: string (filepath), file path (including file name)
+    (function, symbol, interval, *optionalparams): see AlphaVantage documentation
+    '''
+    fullfilename = file
+    if query_switch == True:
+        if optionalparams:
+            url = 'https://www.alphavantage.co/query?'+'function='+str(function)+'&'+'symbol='+str(symbol)+str(optionalparams[0])+'&'+'apikey='+str(apikey)+'&datatype=csv'
+        else:
+            url = 'https://www.alphavantage.co/query?'+'function='+str(function)+'&'+'symbol='+str(symbol)+'&'+'apikey='+str(apikey)+'&datatype=csv'
         data = pd.read_csv(url)
         data.to_csv(fullfilename, index=False)
     if query_switch == False:
